38aa91e61c5d33946504db3e3d0ce405780399cd
Samarth100Coder <kumarsamarth272@gmail.com>
2024-11-24 10:24:56 +0530

    commit

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ while not done:
             done=True
     pygame.draw.rect(screen,(0,125,255), pygame.Rect(30,30,350,250))
     pygame.display.flip()
+    pygame.draw.circle(screen,(0,255,0),(300,300),50)
+    pygame.draw.circle(screen,(0,0,255),(300,300),50,3)
